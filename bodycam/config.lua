f4254e4fc519d358598bc7ba87b4344fe7236efe
Francy-Dev33 <158182990+Francy-Dev33@users.noreply.github.com>
2024-02-04 11:37:40 +0100

    Add Boydcam Scripts

---
+++ bodycam/config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.bodycamSetting = {
+    types = {"standard", "compact", "HD"},
+    resolution = {"720p", "1080p", "4"}
+}
---
+++ bodycam/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Astro RP'
+description 'Script per la bodycam'
+version '1.0'
+
+client_scripts {
+    'client.lua',
+    
+}
