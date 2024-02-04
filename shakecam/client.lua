a614dc7ed4cdb8bd72db9ebdf6dd7653ea1ebf57
Francy-Dev33 <158182990+Francy-Dev33@users.noreply.github.com>
2024-02-04 01:01:36 +0100

    Update ShakeCamera Add ShakeCamEnabled

--- shakecam/client.lua
+++ shakecam/client.lua
@@ -1,15 +1,36 @@
 local shakeDuration  = 500
 local shakeAmplitude = 1.1
+local shakeCamEnabled = true
 
-function ShakeCamera()
+RegisterNetEvent('pkayterCrashed') 
+AddEventHandler('playerCrashed', function()
+    shakeCamEnabled = true
     ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', shakeAmplitude)
-    Wait(shakeDuration)
-    StopGameplayCamShaking(true)
-end
+    SetTimecycleModifier("spectator5")
+    SetPedMotionBlur(PlayerPedId(), true)
 
-RegisterNetEvent('incidente')
-AddEventHandler('incidente', function()
-    ShakeCamera()
-end)
--- Chiamare questo codice quando si verifica un incidente
-TriggerEvent('incidente')
+    SetPedMovementClipset(PlayerPedId(), "MOVE_M@DRUNK@VERYDRUNK", true)
+    Citizen.Wait(shakeDuration)
+    shakeCamEnabled = false 
+    SetTimecycleModifier("")
+    SetPedMotionBlur(PlayerPedId(), false) 
+    ResetPedMovementClipset(PlayerId(), 0.0)
+ end
+)
+
+Citizen.CreateThread(function()
+     while true do 
+        Citizen.Wait(0)
+        if shakeCamEnabled then 
+
+            ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', shakeAmplitude) 
+            local playerPed = PlayerPedId()
+            SetEntityLocallyInvisible(playerPed) 
+            Citizen.Wait(100) 
+            SetEntityLocallyInvisible(playerPed)
+            Citizen.Wait(100)
+
+end
+end
+end
+)
